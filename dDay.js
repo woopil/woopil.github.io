4c9d52ff387fb94803b289b444054854d4e188d2
woopil <57129866+woopil@users.noreply.github.com>
2019-11-04 14:49:28 +0900

    Update dDay.js

--- dDay.js
+++ dDay.js
@@ -6,6 +6,7 @@ setInterval(function(){
     var now = new Date().getTime();
     var distanceXmas = countDownDateXmas - now;
     var distanceTicket = countDownDateTicket - now;
+    var distanceHome = countDownDateTicket - now;
 
     // 일, 시, 분, 초를 등록
     var dX = Math.floor(distanceXmas / (1000 * 60 * 60 * 24));
@@ -18,7 +19,13 @@ setInterval(function(){
     var mT = Math.floor((distanceTicket % (1000 * 60 * 60)) / (1000 * 60));
     var sT = Math.floor((distanceTicket % (1000 * 60)) / 1000);
   
+    var dH = Math.floor(distanceHome / (1000 * 60 * 60 * 24));
+    var hH = Math.floor((distanceHome % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var mH = Math.floor((distanceHome % (1000 * 60 * 60)) / (1000 * 60));
+    var sH = Math.floor((distanceHome % (1000 * 60)) / 1000);
+  
     document.getElementById("d-dayXmas").innerHTML = "X-Mas : " + dX +"일 " + hX + "시간 " + mX + "분 " + sX + "초 남았습니다.";
     document.getElementById("d-dayTicket").innerHTML = "Buy Ticket : " + dT +"일 " + hT + "시간 " + mT + "분 " + sT + "초 남았습니다.";
+    document.getElementById("d-dayTicket").innerHTML = "Home : " + dH +"일 " + hH + "시간 " + mH + "분 " + sH + "초 남았습니다.";
 });
 
