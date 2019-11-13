7b1078bc881a5e79af0279851d7621a56c21a1fc
woopil <57129866+woopil@users.noreply.github.com>
2019-11-13 11:24:37 +0900

    Update dDay.js

--- dDay.js
+++ dDay.js
@@ -1,12 +1,13 @@
 //디데이 종료 일자 설정
 var countDownDateXmas = new Date("Dec 25, 2019 00:00:01").getTime();
-var countDownDateTicket = new Date("Nov 5, 2019 14:00:00").getTime();
 var countDownDateHome = new Date("Nov 18, 2019 10:00:00").getTime();
+var countDownDateTrans = new Date("Mar 31, 2020 00:00:01").getTime();
   
 setInterval(function(){
     var now = new Date().getTime();
     var distanceXmas = countDownDateXmas - now;    
     var distanceHome = countDownDateHome - now;
+    var distanceTrans = countDownDateTrans - now;
 
     // 일, 시, 분, 초를 등록
     var dX = Math.floor(distanceXmas / (1000 * 60 * 60 * 24));
@@ -19,7 +20,13 @@ setInterval(function(){
     var mH = Math.floor((distanceHome % (1000 * 60 * 60)) / (1000 * 60));
     var sH = Math.floor((distanceHome % (1000 * 60)) / 1000);
   
+    var dT = Math.floor(distanceHome / (1000 * 60 * 60 * 24));
+    var hT = Math.floor((distanceHome % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var mT = Math.floor((distanceHome % (1000 * 60 * 60)) / (1000 * 60));
+    var sT = Math.floor((distanceHome % (1000 * 60)) / 1000);    
+  
     document.getElementById("d-dayXmas").innerHTML = "X-Mas : " + dX +"일 " + hX + "시간 " + mX + "분 " + sX + "초 남았습니다.";    
     document.getElementById("d-dayHome").innerHTML = "Home : " + dH +"일 " + hH + "시간 " + mH + "분 " + sH + "초 남았습니다.";
+    document.getElementById("d-dayTrans").innerHTML = "Bye : " + dT +"일 " + hT + "시간 " + mT + "분 " + sT + "초 남았습니다.";
 });
 
