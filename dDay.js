009f3b1e1a4e30e506d37fb134df9373210b31a8
woopil <57129866+woopil@users.noreply.github.com>
2021-01-05 13:31:31 +0900

    Update dDay.js

--- dDay.js
+++ dDay.js
@@ -1,6 +1,5 @@
 //디데이 종료 일자 설정
-var countDownDateXmas = new Date("Dec 25, 2019 00:00:01").getTime();
-var countDownDateTrans = new Date("Mar 31, 2020 00:00:01").getTime();
+var countDownDateTrans = new Date("June 31, 2020 00:00:01").getTime();
   
 setInterval(function(){
     var now = new Date().getTime();
@@ -18,7 +17,6 @@ setInterval(function(){
     var mT = Math.floor((distanceTrans % (1000 * 60 * 60)) / (1000 * 60));
     var sT = Math.floor((distanceTrans % (1000 * 60)) / 1000);    
   
-    document.getElementById("d-dayXmas").innerHTML = "X-Mas : " + dX +"일 " + hX + "시간 " + mX + "분 " + sX + "초 남았습니다.";        
     document.getElementById("d-dayTrans").innerHTML = "Bye : " + dT +"일 " + hT + "시간 " + mT + "분 " + sT + "초 남았습니다.";
 });
 
